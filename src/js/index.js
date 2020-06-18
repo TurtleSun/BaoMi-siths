f35d7b590fee89f7b1c01f9082423ccaa63c0b2e
TurtleSun <57500228+TurtleSun@users.noreply.github.com>
2020-06-18 14:53:06 -0400

    added js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+
+
 var slideIndex = 0;
 carousel();
 
@@ -12,3 +14,30 @@ function carousel() {
   x[slideIndex-1].style.display = "block";
   setTimeout(carousel, 3000); // Change image every 2 seconds
 }
+
+var eachSlide = 1;
+showSlides(eachSlide);
+
+function plusSlides(n) {
+  showSlides(eachSlide += n);
+}
+
+function currentSlide(n) {
+  showSlides(eachSlide = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {eachSlide = 1}
+    if (n < 1) {eachSlide = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[eachSlide-1].style.display = "block";
+  dots[eachSlide-1].className += " active";
+} 
